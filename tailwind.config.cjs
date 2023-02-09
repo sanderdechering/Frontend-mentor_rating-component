e0330ee80b72ee8cb1a178137808881239c739be
sander dechering <sander.dechering@gmail.com>
2023-02-09 11:16:06 +0100

    Styleguide integration
    Added styleguide colors and font to the project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-orange' : 'hsl(25, 97%, 53%)',
+        'my-white' : 'hsl(0, 0%, 100%)',
+        'my-light-gray' : 'hsl(217, 12%, 63%)',
+        'my-medium-gray' : 'hsl(216, 12%, 54%)',
+        'my-dark-blue' : 'hsl(213, 19%, 18%)',
+        'my-very-dark-blue' : 'hsl(216, 12%, 8%)'
+      }
+
+    },
   },
   plugins: [],
 }
